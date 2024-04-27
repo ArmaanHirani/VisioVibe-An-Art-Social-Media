031f3f65badc0ef938603fc86cfd6756824c5f4c
Armaan Hirani <ahirani3@student.gsu.edu>
2024-04-27 02:25:20 -0400

    Made the bottomAppBar tabs go to their respective pages

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project_2/chat.dart';
+import 'package:project_2/profile.dart';
+import 'package:project_2/search.dart';
 
 class homepage extends StatelessWidget {
   @override
@@ -27,6 +30,30 @@ class homepage extends StatelessWidget {
             label: "Chat",
           ),
         ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SearchPage()),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatPage()),
+              );
+              break;
+            default:
+              break;
+          }
+        },
       ),
     );
   }
