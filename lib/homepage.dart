f41b67e8af3e50c55e9750ad4c8e6a16f5e49f14
Armaan Hirani <ahirani3@student.gsu.edu>
2024-04-27 00:26:14 -0400

    added basic homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("HOMEPAGE"),
+        ),
+      ),
+      body: Center(
+        child: Text("Welcome to Homepage"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Chat",
+          ),
+        ],
+      ),
+    );
   }
-
 }
