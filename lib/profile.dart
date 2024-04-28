b6a16454591d8f214b0c8b972aca696ce0386e3d
Armaan Hirani <ahirani3@student.gsu.edu>
2024-04-27 21:53:48 -0400

    the start of the profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_2/login.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -6,6 +9,40 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  String _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchEmail();
+  }
+
+  Future<void> _fetchEmail() async {
+    User? user = _auth.currentUser;
+    if (user != null && mounted) {
+      DocumentSnapshot snapshot =
+          await _firestore.collection('users').doc(user.uid).get();
+      if (snapshot.exists) {
+        setState(() {
+          _email = (snapshot.data() as Map<String, dynamic>)['email'];
+        });
+      }
+    }
+  }
+
+  Future<void> _signOut() async {
+    try {
+      await _auth.signOut();
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +50,23 @@ class _ProfilePageState extends State<ProfilePage> {
         title: Text("Profile"),
       ),
       body: Center(
-        child: Text("Profile Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_email, style: TextStyle(fontSize: 24)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _signOut,
+              child: Text('Log Out'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
